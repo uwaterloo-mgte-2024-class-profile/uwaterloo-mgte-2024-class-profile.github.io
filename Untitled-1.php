bfc618a4aa934058ba76c1e19f19028acdc80fe0
PurpleZenith <purplezenith727@gmail.com>
2025-03-23 21:05:38 -0400

    changing html5 template

---
+++ Untitled-1.php
@@ -0,0 +1,82 @@
+
+
+	<!-- Two -->
+	<section id="two" class="main style2">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<ul class="major-icons">
+						<li><span class="icon solid style1 major fa-code"></span></li>
+						<li><span class="icon solid style2 major fa-bolt"></span></li>
+						<li><span class="icon solid style3 major fa-camera-retro"></span></li>
+						<li><span class="icon solid style4 major fa-cog"></span></li>
+						<li><span class="icon solid style5 major fa-desktop"></span></li>
+						<li><span class="icon solid style6 major fa-calendar"></span></li>
+					</ul>
+				</div>
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h2>Lorem ipsum dolor adipiscing<br />
+							amet dolor consequat</h2>
+					</header>
+					<p>foobar!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Three -->
+	<section id="three" class="main style1 special">
+		<div class="container">
+			<header class="major">
+				<h2>Adipiscing amet consequat</h2>
+			</header>
+			<p>Ante nunc accumsan et aclacus nascetur ac ante amet sapien sed.</p>
+			<div class="row gtr-150">
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><img src="images/pic02.jpg" alt="" /></span>
+					<h3>Magna feugiat lorem</h3>
+					<p>foobar!</p>
+				</div>
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><img src="images/pic03.jpg" alt="" /></span>
+					<h3>Magna feugiat lorem</h3>
+					<p>foobar!</p>
+				</div>
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><img src="images/pic04.jpg" alt="" /></span>
+					<h3>Magna feugiat lorem</h3>
+					<p>foobar!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Four -->
+	<section id="four" class="main style2 special">
+		<div class="container">
+			<header class="major">
+				<h2>Ipsum feugiat consequat?</h2>
+			</header>
+			<p>Sed lacus nascetur ac ante amet sapien.</p>
+			<ul class="actions special">
+				<li><a href="#" class="button wide primary">Sign Up</a></li>
+				<li><a href="#" class="button wide">Learn More</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<section id="footer">
+		<ul class="icons">
+			<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+			<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+			<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+		</ul>
+		<ul class="copyright">
+			<li>&copy; Untitled</li>
+			<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+		</ul>
+	</section>
